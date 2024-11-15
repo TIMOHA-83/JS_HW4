4c09d550fabe2ee00a2f379940698702aac58dd3
TIMOHA-83 <timmuch2003@gmail.com>
2024-11-15 09:38:21 +0200

    update main1

--- HW1_#I2XsG6f/main.js
+++ HW1_#I2XsG6f/main.js
@@ -1,1 +1,10 @@
 //- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+function rectangle(a, b){
+    return a*b;
+}
+let a=+prompt('enter a')
+let b=+prompt('enter b')
+
+let s = rectangle(a, b);
+console.log(s);
