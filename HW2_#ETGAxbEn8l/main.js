8d94e07855c82b019a9be7d1a146c7f0e02a3c51
Timofey Mukha <71269411+TIMOHA-83@users.noreply.github.com>
2024-11-15 10:46:23 +0200

    Update main.js in HW2

--- HW2_#ETGAxbEn8l/main.js
+++ HW2_#ETGAxbEn8l/main.js
@@ -1,1 +1,10 @@
 //- створити функцію яка обчислює та повертає площу кола з радіусом r
+function circle(r){
+    const p=3.14;
+    return p*(r*r);
+}
+
+let r=+prompt('enter r');
+
+let s = circle(r);
+console.log(s);
